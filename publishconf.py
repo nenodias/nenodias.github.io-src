da5d62f9010affb4bd1c4f5af74c95732db834f8
Horácio Dias <horacio.baptista@supportweb.com>
2016-11-11 14:13:09 -0200

    Adicionando tema

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://nenodias.github.io'
+SITEURL = 'https://nenodias.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
