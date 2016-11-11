866a7caef19234357146723b1df6c49524a95fad
Horácio Dias <horacio.baptista@supportweb.com>
2016-11-11 14:05:10 -0200

    Primeiro commit dos sources

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Horácio Dias Baptista Neto'
+SITENAME = 'Nenodias'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Sao_Paulo'
+
+DEFAULT_LANG = 'pt'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('TryPix', 'http://trypix.wordpress.com/'),
+         ('You Tube', 'http://youtube.com/'),)
+
+# Social widget
+SOCIAL = (('Github', 'http://github.com/nenodias'),
+          ('Linkedin', 'https://www.linkedin.com/in/nenodias92'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+THEME= '/home/hdias/html/pelican-cait/'
