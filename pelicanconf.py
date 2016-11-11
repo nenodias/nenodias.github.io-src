0a8eb4e1ab0d6303f8b7bcb9d0903a4f634ece47
Horácio Dias <horacio.baptista@supportweb.com>
2016-11-11 14:15:56 -0200

    Finalizando adição do thema padrão usando pelican

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,4 +31,4 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
-THEME= '/home/hdias/html/pelican-cait/'
+THEME= 'theme/'
